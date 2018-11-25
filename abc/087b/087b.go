54c18e9a625880fd02de64d09e65122994e8bc1d
Fumitaka Tagawa <fumufumi@gmail.com>
2018-11-26 06:07:28 +0900

    途中

---
+++ abc/087b/087b.go
@@ -0,0 +1,13 @@
+package main
+
+
+func main(){
+    var a, b, c, total int
+    fmt.Scanf("%d", &a)
+    fmt.Scanf("%d", &b)
+    fmt.Scanf("%d", &c)
+    fmt.Scanf("%d", &total)
+
+    max_a := 
+    
+}
