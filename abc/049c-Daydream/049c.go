24412356a5386e85b44c70122b5fb1d036723d93
Fumitaka Tagawa <fumufumi@gmail.com>
2018-12-01 14:43:03 +0900

    文字列の読み込みに時間がかかるところを修正。別解追加

--- abc/049c-Daydream/049c.go
+++ abc/049c-Daydream/049c.go
@@ -1,6 +1,9 @@
 package main
 
 import "fmt"
+import "strings"
+import "bufio"
+import "os"
 
 const (
     DREAM = "dream"
@@ -11,8 +14,10 @@ const (
 
 func main(){
     var str string
-    fmt.Scanf("%s", &str)
-
+    //長い文字列だと時間がかかる
+    //fmt.Scanf("%s", &str)
+    //こっちのほうが早い
+    str, _ = readLine()
     if (checkWords(str)){
         fmt.Println("YES")
     }else{
@@ -20,6 +25,25 @@ func main(){
     }
 }
 
+
+var rdr = bufio.NewReaderSize(os.Stdin, 100000)
+
+func readLine() (string, error) {
+    buf := make([]byte, 0, 100000)
+    for {
+        l, p, e := rdr.ReadLine()
+        if e!=nil {
+            return "", e
+        }
+        buf = append(buf, l...)
+        if !p {
+            break
+        }
+    }
+    return string(buf), nil
+}
+
+/*
 func checkWords(s string) bool{
     len_s := len(s)
     if len_s == 0 {
@@ -38,4 +62,25 @@ func checkWords(s string) bool{
     }else{
         return (checkWords(s[0:5]) && checkWords(s[5:])) || (checkWords(s[0:7]) && checkWords(s[7:])) || (checkWords(s[0:6]) && checkWords(s[6:]))
     }
+}
+*/
+
+//別解 末尾から長い単語の順番でtrimしていく
+func checkWords(s string) bool {
+    t := s
+    for {
+        s = strings.TrimSuffix(s, DREAMER)
+        s = strings.TrimSuffix(s, ERASER)
+        s = strings.TrimSuffix(s, ERASE)
+        s = strings.TrimSuffix(s, DREAM)
+
+        switch s {
+        case ""://空になった
+            return true
+        case t://トリムできなかった
+            return false
+        }
+        t = s
+    }
+    
 }
