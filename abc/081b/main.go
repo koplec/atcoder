f150289bd853779ccb0fae4e22a83f7425d05194
fumitaka tagawa <f_tagawa@si-seiko.com>
2018-12-15 17:00:01 +0900

    scanがなかったので動かなかった。つらい。

--- abc/081b/main.go
+++ abc/081b/main.go
@@ -25,14 +25,3 @@ loop:
 	}
 	fmt.Printf("%d\n", count)
 }
-
-/**
- * １行に空白区切りで数字を読み込み
- */
-func scanNums(len int) (nums []int) {
-	nums = make([]int, len)
-	for i := 0; i < len; i++ {
-		fmt.Scan(&nums[i])
-	}
-	return
-}
