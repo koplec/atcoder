2e0a14ff7fccb39e7364eea496e5672f0ed18a5b
fumitaka tagawa <f_tagawa@si-seiko.com>
2018-12-17 18:56:56 +0900

    agc029Bを解いたが、正解できない。イレギュラーパターンを探す必要がある。

--- agc/029/b/agc029b.go
+++ agc/029/b/agc029b.go
@@ -4,86 +4,68 @@ import (
 	"fmt"
 	"math"
 	"os"
+	"sort"
 )
 
+//解説動画を見て解きなおす
+//大きい順にソートして、大きい数と2^tを作る数がないか調べる
+//あったら、それをpairsに追加して、大きい数とその数を消す。
+//空になるまで続ける。
 func main() {
 
 	var N int
 	fmt.Scanf("%d", &N)
+	nums := scanNums(N)
+	fmt.Printf("%d\n", countPair(nums))
 
-	a := scanNums(N)
-
-	pairs := make([][2]int, 0)
-
-	for i := 0; i < N; i++ {
-		for j := i + 1; j < N; j++ {
-			sum := a[i] + a[j]
-			if isPow2(sum) {
-				pair := [2]int{i, j}
-				pairs = append(pairs, pair)
-			}
-		}
-	}
-
-	pairLen := len(pairs)
-	permutations := genPermutation(pairLen)
-	maxBallCount := 0
-	var maxBallMap map[int]bool
-	for _, p := range permutations {
-		ballMap := make(map[int]bool)
-		for _, i := range p {
-			idx := i - 1
-			apair := pairs[idx]
-			a, b := apair[0], apair[1]
-			//既に登録済みだったらカウントしない
-			if _, ok := ballMap[a]; ok {
-				continue
-			}
-			if _, ok := ballMap[b]; ok {
-				continue
-			}
-			ballMap[a] = true
-			ballMap[b] = true
-		}
-		ballCount := len(ballMap)
-		if maxBallCount < ballCount {
-			maxBallCount = ballCount
-			maxBallMap = ballMap
-		}
-	}
-	debug("ballMap -> %v\n", maxBallMap)
-	fmt.Printf("%d\n", maxBallCount/2)
 }
 
-//n!個のpermutationを作る
-func genPermutation(n int) [][]int {
-	if n == 1 {
-		ret := make([][]int, 0)
-		ret = append(ret, []int{1})
-		return ret
-	} else {
-		permutations := genPermutation(n - 1)
-		ret := make([][]int, 0)
-		for _, p1 := range permutations {
-			for idx := 0; idx < n-1; idx++ {
-				tmpp1 := make([]int, len(p1))
-				copy(tmpp1, p1)
-				p2 := make([]int, 0)
-				p2 = append(p2, tmpp1[:idx]...)
-				p2 = append(p2, n)
-				p2 = append(p2, tmpp1[idx:]...)
-				ret = append(ret, p2)
-			}
-			tmpp1 := make([]int, len(p1))
-			copy(tmpp1, p1)
-			p2 := make([]int, 0)
-			p2 = append(p2, tmpp1...)
-			p2 = append(p2, n)
+func countPair(nums []int) int {
 
-			ret = append(ret, p2)
+	//大きい順に並べる
+	sort.Sort(sort.Reverse(sort.IntSlice(nums)))
+
+	//値からスライスのindexへアクセスするためのmap
+	numIndex := make(map[int]int)
+	for idx, num := range nums {
+		numIndex[num] = idx
+	}
+
+	pairCount := 0
+	maxIndex := 0
+	for maxIndex < len(nums) {
+		debug("nums = %v\n", nums)
+		num0 := nums[maxIndex]
+		if num0 < 0 { //すでにペアになって印がついている
+			maxIndex++
+			continue
 		}
+		pow2 := ceilPow2(num0)
+		num1 := pow2 - num0
+		debug("nums0=%d pow2=%d num1=%d\n", num0, pow2, num1)
+		if numIndex, ok := numIndex[num1]; ok {
+			if nums[numIndex] > 0 {
+				debug("pair=(%d,%d)\n", num0, num1)
+				pairCount++
+				nums[numIndex] = -1 //一度ペアになったら印をつける
+			}
+		}
+		nums[maxIndex] = -1 //念のためmaxIndexのところに-1を付けておく
+		maxIndex++
+	}
+	return pairCount
+}
 
-		return ret
+/**
+ * 引数で与えられた数以上の2の累乗数を求める
+ */
+func ceilPow2(n int) int {
+	t := math.Log2(float64(n))
+	pow2 := 1 << uint64(t)
+	if n == pow2 {
+		return n
+	} else {
+		return pow2 << 1
 	}
 }
 
@@ -96,11 +78,6 @@ func scanNums(len int) (nums []int) {
 		fmt.Scan(&nums[i])
 	}
 	return
-}
-
-func isPow2(n int) bool {
-	t := math.Log2(float64(n))
-	return (1 << uint64(t)) == uint64(n)
 }
 
 //** DEBUG **/
