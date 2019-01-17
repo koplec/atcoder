08d23ac5038e56b9be953c4acbdda11c6308e32f
fumitaka tagawa <f_tagawa@si-seiko.com>
2019-01-17 14:38:57 +0900

    2問違い

---
+++ abc/110/110b/110b.go
@@ -0,0 +1,56 @@
+package main
+
+import "fmt"
+
+func main() {
+	var tmp []int
+	tmp = scanNums(4)
+	N := tmp[0]
+	M := tmp[1]
+	X := tmp[2]           //xの首都の座標
+	Y := tmp[3]           //yの首都の座標
+	xslice := scanNums(N) //xの領土の座標
+	yslice := scanNums(M) //yの領土の座標
+
+	fmt.Printf("%s\n", solve(X, Y, xslice, yslice))
+}
+
+func solve(X, Y int, xslice, yslice []int) string {
+	_, xmax := searchMinMax(xslice)
+	ymin, _ := searchMinMax(yslice)
+
+	if !(xmax <= ymin) {
+		return "War"
+	}
+
+	if X < ymin && ymin <= Y {
+		return "No War"
+	}
+	return "War"
+
+}
+
+/**
+ * １行に空白区切りで数字を読み込み
+ */
+func scanNums(len int) (nums []int) {
+	nums = make([]int, len)
+	for i := 0; i < len; i++ {
+		fmt.Scan(&nums[i])
+	}
+	return
+}
+
+func searchMinMax(nums []int) (int, int) {
+	min := 10000
+	max := -10000
+	for _, n := range nums {
+		if n > max {
+			max = n
+		}
+		if n < min {
+			min = n
+		}
+	}
+	return min, max
+}
