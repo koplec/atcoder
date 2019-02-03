53b61bbbed62c89532c9024401d7bd568b5883c2
Fumitaka Tagawa <fumufumi@gmail.com>
2019-02-03 22:48:12 +0900

    うーん時間内に溶けなかった

--- abc/117/117d/117d.go
+++ abc/117/117d/117d.go
@@ -2,6 +2,8 @@ package main
 
 import "fmt"
 import "os"
+import "sort"
+import "strconv"
 
 func main(){
     var N, K int
@@ -20,13 +22,37 @@ func main(){
 }
 
 func solve(nums []int, K int) (int, int){
+    //numsをソート
+    //最大の数
+    sort.Sort(sort.Reverse(sort.IntSlice(nums)))
+    maxNum := nums[0]
+    //バイナリ表記にする
+    //最大桁数を計算する
+    maxNumBinStr := fmt.Sprintf("%b", maxNum)
+    maxBinNum := len(maxNumBinStr)
+    
+    maxKStr := ""
+    for bitPos := maxBinNum; bitPos >= 0; bitPos--{
+        zeroNum := 0
+        oneNum := 0
+        for _, num := range nums{
+            if refbit(num, bitPos) == 1{
+                oneNum++
+            }else{
+                zeroNum++
+            }
+        }
+        if oneNum > zeroNum {
+            maxKStr += "0"
+        }else{
+            maxKStr += "1"
+        }
+    }
+    
     max := -1
     maxK := -1
     for k:=0; k<=K; k++{
-        total := 0
-        for _, n := range nums {
-            total += xor(k, n)
-        }
+        total := f(k, nums)
         if total > max {
             maxK = k
             max = total
@@ -34,6 +60,21 @@ func solve(nums []int, K int) (int, int){
     }
     return max, maxK
 }
+
+//任意のビット位置の値を参照する
+func refbit(i int, b uint) int {
+    return (i >> b) & 1
+}
+
+func f(x int, nums []int) int{
+    total := 0
+    for _, n := range nums {
+        total += xor(x, n)
+    }
+    return total
+}
+
+//
 
 func xor(a, b int) int{
     return a ^ b
