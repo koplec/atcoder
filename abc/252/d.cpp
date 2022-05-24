0a23efc6bd1c78c05cb5636807d6b377bc9d53c6
koplec <koplec@github.com>
2022-05-24 19:13:39 +0900

    配列からvectorにしたけど遅い

--- abc/252/d.cpp
+++ abc/252/d.cpp
@@ -2,12 +2,19 @@
 #include <vector>
 #include <iostream>
 
-const int MAX = 200000;
-
-int N;
-int A[MAX];
 int count;
-void iter(int index, const int A[], std::vector<int> numVec){
+void iter(int index, int N, std::vector<int>& A, std::vector<int> numVec){
+    if(index >= N){
+        return;
+    }
+
+    if(numVec.size() == 2){
+        int a = numVec.at(0);
+        int b = numVec.at(1);
+        if(a == b){
+            return;
+        }
+    }
     if(numVec.size() == 3){
         int a = numVec.at(0);
         int b = numVec.at(1);
@@ -19,31 +26,25 @@ void iter(int index, const int A[], std::vector<int> numVec){
             return;
         }
     }
-    if(numVec.size() == 2){
-        int a = numVec.at(0);
-        int b = numVec.at(1);
-        if(a == b){
-            return;
-        }
-    }
-    if(index >= N){
-        return;
-    }
+
 
     //
-    iter(index+1, A, numVec);
+    iter(index+1, N, A, numVec);
     numVec.push_back(A[index]);
-    iter(index+1, A, numVec);
+    iter(index+1, N, A, numVec);
 }
 
 int main(){
+    int N;
     scanf("%d", &N);
-    for(int i=0; i<N; i++) scanf("%d", &A[i]);
+    std::vector<int> A(N);
+    for(int i=0; i<N; i++){
+        std::cin >> A.at(i);
+    }
 
     std::vector<int> numVec;
 
-    iter(0, A, numVec);
+    iter(0,N, A, numVec);
     std::cout << count <<std::endl;
     return 0;
-
 }
