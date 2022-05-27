7efd3b4c568d3be879982050620cfbaf2155f861
koplec <koplec@github.com>
2022-05-28 00:38:18 +0900

    解けないからassertでテストケースを書いて検討したけどやっぱりわからない

--- abc/252/d-dp.cpp
+++ abc/252/d-dp.cpp
@@ -3,81 +3,12 @@
 #include <iostream>
 #include <map>
 #include <algorithm>
+#include <cassert>
 
 using namespace std;
 
-int countComb(map<int, int> numCount, int n)
-{
-    if (n <= 2)
-    {
-        int countSum = 0;
-        int countSum2 = 0;
-        for (auto t : numCount)
-        {
-            int c = t.second;
-            countSum += c;
-            countSum2 += c * c;
-        }
 
-        if (n == 1)
-        {
-            return countSum;
-        }
-        if (n == 2)
-        {
-            return countSum * countSum - countSum2;
-        }
-    }
-
-    // n==3
-    if(numCount.size()<=2){
-        return 0;
-    }
-
-
-    auto target = numCount.begin();
-
-    int targetNum = target->first;
-    int targetCount = target->second;
-
-    int count = 0;
-    map<int, int> tmpMap; // targetを除いたmap作成
-    for (auto t : numCount)
-    {
-        if (t.first != targetNum)
-        {
-            tmpMap[t.first] = t.second;
-        }
-    }
-    count = targetCount * countComb(tmpMap, 2) + countComb(tmpMap, 3);
-    return count;
-}
-
-int main()
-{
-    int N;
-    scanf("%d", &N);
-
-    //map
-    map<int, int> numCount;
-    vector<int> nums;
-    for (int i = 0; i < N; i++)
-    {
-        int a;
-        cin >> a;
-        if (numCount.count(a))
-        {
-            numCount[a]++;
-        }
-        else
-        {
-            numCount[a] = 1;
-            nums.push_back(a);
-        }
-    }
-    //numsを昇順でsort
-    sort(nums.begin(), nums.end());
-
+int count(map<int, int> numCount, vector<int> nums){
     //dpを準備
     //numsを順番に並べる
     //dp[i][j]：i番目の数字まで確定して、集合のサイズがjである場合の数
@@ -113,7 +44,97 @@ int main()
         }
     }
 
-    cout << dp[nums.size()-1][3] << endl;
+    return dp[nums.size()-1][3];
+}
+
+void assertCount(string title, vector<int> A, int expected){
+    map<int, int> numCount;
+    vector<int> nums;
+    for (int i = 0; i < A.size(); i++)
+    {
+        int a = A.at(i);
+        if(numCount.count(a))
+        {
+            numCount[a]++;
+        }
+        else
+        {
+            numCount[a] = 1;
+            nums.push_back(a);
+        }
+    }
+    int cnt = count(numCount, nums);
+    if(cnt != expected){
+        cout << "ERROR!! "  << title << " failed cnt:" <<cnt << ", expected:" << expected << endl;
+    }
+}
+
+
+int main()
+{
+    vector<int> a0 {1, 2, 3};
+    assertCount("A0", a0, 1);
+
+    vector<int> a1 {3, 1, 4, 1};
+    assertCount("A1", a1, 2);
+
+    vector<int> a2 {99999, 99998, 99997, 99996, 99995, 99994, 99993, 99992, 99991,99990};
+    assertCount("A2", a2, 120);
+    
+    vector<int> a3 {3,1,4,1,5,9,2,6,5,3,5,8,9,7,9};
+    assertCount("A3", a3, 355);
+    
+    vector<int> b0 {1, 1, 1};
+    assertCount("B0", b0, 0);
+    
+    vector<int> b1 {1, 2, 2};
+    assertCount("B1", b1, 0);
+    
+    vector<int> b2 {1, 1, 1, 1, 1, 1, 1, 1, 1};
+    assertCount("B2", b2, 0);
+
+    vector<int> b3 {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 3};
+    assertCount("B3", b3, 10);
+
+    vector<int> b4 {1, 2, 3, 4, 5};
+    assertCount("B4", b4, 10);
+
+    vector<int> b5 {1, 2, 3, 4, 5, 5};
+    assertCount("B5", b5, 4 + 2*6);
+
+    vector<int> b6 {1, 2, 3, 3, 4, 5};
+    assertCount("B6", b6, 4 + 2*6);
+
+    vector<int> b7 {1, 2, 2,  3, 4, 5};
+    assertCount("B7", b7, 4 + 2*6);
 
     return 0;
+    //本番用
+    // int N;
+    // scanf("%d", &N);
+
+    // //map
+    // map<int, int> numCount;
+    // vector<int> nums;
+    // for (int i = 0; i < N; i++)
+    // {
+    //     int a;
+    //     cin >> a;
+    //     if (numCount.count(a))
+    //     {
+    //         numCount[a]++;
+    //     }
+    //     else
+    //     {
+    //         numCount[a] = 1;
+    //         nums.push_back(a);
+    //     }
+    // }
+    // //numsを昇順でsort
+    // sort(nums.begin(), nums.end());
+
+    // int ret = count(numCount, nums);
+    // cout << ret << endl;
+
+    // return 0;
 }
