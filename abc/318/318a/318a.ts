3131f450eb2c0b0cb3305371fc05417327065746
koplec <koplec@github.com>
2023-09-12 09:33:48 +0900

    format調整

--- abc/318/318a/318a.ts
+++ abc/318/318a/318a.ts
@@ -1,23 +1,21 @@
 import * as fs from "fs";
 
 const DEBUG = false;
-if(!DEBUG){
+if (!DEBUG) {
   console.debug = () => {};
 }
 
-
-const str: string= fs.readFileSync("/dev/stdin", "utf8");
+const str: string = fs.readFileSync("/dev/stdin", "utf8");
 console.debug("stdin:", str);
-
 
 // 1日目からN日目まで満月を見れる日を出力
 // 満月はM日から　M+P, M+2P, で見れる
-const [N, M, P]:number[] = str.split(' ').map((str) => Number(str));
-console.debug(N, M, P)
+const [N, M, P]: number[] = str.split(" ").map((str) => Number(str));
+console.debug(N, M, P);
 
 let count = 0;
-for (let i = M; i <= N; i=i+P) {
-  count++
+for (let i = M; i <= N; i = i + P) {
+  count++;
 }
 
-console.log(count)
+console.log(count);
