dca487a19d6656eff1340ef1ff958f9f9875c158
koplec <koplec@github.com>
2023-09-26 16:00:20 +0900

    197b一旦コミット

--- abc/197/197b/197b.test.ts
+++ abc/197/197b/197b.test.ts
@@ -1,6 +1,6 @@
 import { test, describe, expect } from "@jest/globals";
 
-import { canSeeMasu } from "./197b";
+import { canSeeMasu, compareMasuPoint, genMasuMap } from "./197b";
 
 describe("array equality check", () => {
   test("number[][]", () => {
@@ -18,65 +18,163 @@ describe("array equality check", () => {
   });
 });
 
-describe("197b test", () => {
-  test("init attempt", () => {
-    expect(canSeeMasu(0, 0, [[0]])).toStrictEqual([[0, 0]]);
+describe("object array sort is stable?", () => {
+  let a = [
+    { x: 0, y: 0 },
+    { x: 0, y: 1 },
+    { x: 1, y: 0 },
+  ];
 
+  let b = [
+    { x: 1, y: 0 },
+    { x: 0, y: 0 },
+    { x: 0, y: 1 },
+  ];
+
+  console.debug(a.sort(compareMasuPoint));
+  console.debug(b.sort(compareMasuPoint));
+
+  expect(a.sort(compareMasuPoint)).toStrictEqual(b.sort(compareMasuPoint));
+});
+
+describe("197b 2block", () => {
+  test("init attempt", () => {
+    expect(canSeeMasu(0, 0, [[0]]).sort(compareMasuPoint)).toStrictEqual(
+      [{ x: 0, y: 0 }].sort(compareMasuPoint)
+    );
+  });
+  test("right", () => {
     expect(
       canSeeMasu(0, 0, [
         [0, 0],
         [1, 1],
-      ])
-    ).toStrictEqual([
-      [0, 0],
-      [0, 1],
-    ]);
-
-    expect(
-      canSeeMasu(0, 0, [
-        [0, 1],
-        [0, 1],
-      ])
-    ).toStrictEqual([
-      [0, 0],
-      [1, 0],
-    ]);
-
-    expect(
-      canSeeMasu(0, 0, [
-        [0, 0],
-        [0, 1],
-      ])
-    ).toStrictEqual([
-      [0, 0],
-      [0, 1],
-      [1, 0],
-    ]);
-
-    expect(
-      canSeeMasu(0, 0, [
-        [0, 0],
-        [0, 1],
-      ]).sort()
+      ]).sort(compareMasuPoint)
     ).toStrictEqual(
       [
-        [0, 0],
-        [1, 0],
-        [0, 1],
-      ].sort()
+        { x: 0, y: 0 },
+        { x: 1, y: 0 },
+      ].sort(compareMasuPoint)
     );
+  });
 
+  test("down", () => {
+    expect(
+      canSeeMasu(0, 0, [
+        [0, 1],
+        [0, 1],
+      ]).sort(compareMasuPoint)
+    ).toStrictEqual(
+      [
+        { x: 0, y: 0 },
+        { x: 0, y: 1 },
+      ].sort(compareMasuPoint)
+    );
+  });
+
+  test("down and right", () => {
+    let expected = canSeeMasu(0, 0, [
+      [0, 0],
+      [0, 1],
+    ]).sort(compareMasuPoint);
+
+    expect(expected).toEqual(
+      [
+        { x: 0, y: 0 },
+        { x: 1, y: 0 },
+        { x: 0, y: 1 },
+      ].sort(compareMasuPoint)
+    );
+  });
+
+  test("down and right 2", () => {
+    expect(
+      canSeeMasu(0, 0, [
+        [0, 0],
+        [0, 1],
+      ]).sort(compareMasuPoint)
+    ).toStrictEqual(
+      [
+        { x: 0, y: 0 },
+        { x: 1, y: 0 },
+        { x: 0, y: 1 },
+      ].sort(compareMasuPoint)
+    );
+  });
+
+  test("down and right 3", () => {
     expect(
       canSeeMasu(0, 0, [
         [0, 0],
         [0, 0],
-      ]).sort()
+      ]).sort(compareMasuPoint)
     ).toStrictEqual(
       [
-        [0, 0],
-        [1, 0],
-        [0, 1],
-      ].sort()
+        { x: 0, y: 0 },
+        { x: 1, y: 0 },
+        { x: 0, y: 1 },
+      ].sort(compareMasuPoint)
     );
+  });
+
+  test("left", () => {
+    let expected = canSeeMasu(1, 0, [
+      [0, 0],
+      [1, 1],
+    ]);
+    expect(expected.sort(compareMasuPoint)).toStrictEqual(
+      [
+        { x: 0, y: 0 },
+        { x: 1, y: 0 },
+      ].sort(compareMasuPoint)
+    );
+  });
+
+  test("up", () => {
+    expect(
+      canSeeMasu(0, 1, [
+        [0, 1],
+        [0, 1],
+      ]).sort(compareMasuPoint)
+    ).toStrictEqual(
+      [
+        { x: 0, y: 0 },
+        { x: 0, y: 1 },
+      ].sort(compareMasuPoint)
+    );
+  });
+});
+
+describe("given sample", () => {
+  test("sample01", () => {
+    const lines = `##..
+...#
+#.#.
+.#.#`.split("\n");
+    const expectedMasuMap = genMasuMap(4, 4, lines);
+    expect(expectedMasuMap).toStrictEqual([
+      [1, 1, 0, 0],
+      [0, 0, 0, 1],
+      [1, 0, 1, 0],
+      [0, 1, 0, 1],
+    ]);
+
+    const expecedCanSeeMasu = canSeeMasu(2 - 1, 2 - 1, expectedMasuMap);
+    expect(expecedCanSeeMasu.length).toBe(4);
+  });
+
+  test("sample02", () => {
+    const lines = `#....
+#####
+....#`.split("\n");
+    const expectedMasuMap = genMasuMap(3, 5, lines);
+    expect(expectedMasuMap).toStrictEqual([
+      [1, 0, 0, 0, 0],
+      [1, 1, 1, 1, 1],
+      [0, 0, 0, 0, 1],
+    ]);
+
+    //0,3で呼び出したい
+    const expectedCanSeeMasu = canSeeMasu(3, 0, expectedMasuMap);
+    expect(expectedCanSeeMasu.length).toBe(4);
   });
 });
--- abc/197/197b/197b.ts
+++ abc/197/197b/197b.ts
@@ -32,51 +32,125 @@ function* zip<T>(...args: Array<T>[]) {
   }
 }
 
-export function canSeeMasu(x: number, y: number, masu: number[][]): number[][] {
-  console.debug("canSeeMasu masu:", masu);
+export class Masu {
+  constructor(private _map: number[][]) {
+    this._map = _map;
+  }
+
+  /**
+   * 障害物か否か
+   * 障害物だったら1, 層じゃないときは0
+   *
+   * @param x 横方向座標
+   * @param y 縦方向座標
+   */
+  value(x: number, y: number): number {
+    return this._map[y][x];
+  }
+  width(): number {
+    return this._map[0]!.length;
+  }
+  height(): number {
+    return this._map!.length;
+  }
+}
+
+export interface MasuPoint {
+  x: number;
+  y: number;
+}
+export const compareMasuPoint = function (a: MasuPoint, b: MasuPoint): number {
+  if (a.x != b.x) return a.x - b.x;
+  if (a.y != b.y) return a.y - b.y;
+  return 0;
+};
+
+type MasuMap = number[][];
+
+export function canSeeMasu(
+  x: number,
+  y: number,
+  masuMap: MasuMap
+): MasuPoint[] {
+  const masu = new Masu(masuMap);
+  console.debug("canSeeMasu masu:", masuMap);
   //結果のマスを[[1,2], [3,4]]のように入れていく
-  const ret: number[][] = new Array();
-  const h = masu.length;
-  const w = masu[0].length;
+  const ret: MasuPoint[] = new Array();
+  const h = masu.height();
+  const w = masu.width();
   console.debug("h:", h);
   console.debug("w:", w);
-  ret.push([x, y]);
+  ret.push({ x: x, y: y });
   //right
-  for (let j = x + 1; j < w && j >= 0; j++) {
-    console.debug(`(i, j)=(${j}, ${y})`);
-    console.debug(`masu value=${masu[y][j]}`);
-    if (masu[y][j] === 1) break;
-    ret.push([y, j]);
+  console.debug("right");
+  for (let i = x + 1; i < w && i >= 0; i++) {
+    if (masu.value(i, y) === 1) break;
+    ret.push({ x: i, y: y });
+  }
+  //left
+  console.debug("left");
+  for (let i = x - 1; i < w && i >= 0; i--) {
+    if (masu.value(i, y) === 1) break;
+    ret.push({ x: i, y: y });
   }
 
   //down
-  for (let i = y + 1; i < h && i >= 0; i++) {
-    console.debug(`(i, j)=(${i}, ${x})`);
-    console.debug(`masu value=${masu[i][x]}`);
-    if (masu[i][x] === 1) break;
-    ret.push([i, x]);
+  for (let j = y + 1; j < h && j >= 0; j++) {
+    console.debug(`(i, j)=(${j}, ${x})`);
+    if (masu.value(x, j) === 1) break;
+    ret.push({ x: x, y: j });
+  }
+
+  //up
+  for (let j = y - 1; j < h && j >= 0; j--) {
+    console.debug(`(i, j)=(${j}, ${x})`);
+    if (masu.value(x, j) === 1) break;
+    ret.push({ x: x, y: j });
   }
 
   console.debug("canSeeMasu:", ret);
   return ret;
+}
+/**
+ *   map作り
+  0 .に対応, 障害物がないことを表す
+  1 #に対応, 障害物があることを表す
+  地図の形を配列に書き換える
+  例えば、
+  ..#..
+  .#..#
+  ##...
+  を
+  [[0,0,1,0,0],
+   [0,1,0,0,1],
+   [1,1,0,0,0]] に変換
+   
+ * @param H 
+ * @param W 
+ * @param lines 
+ * @returns 
+ */
+export function genMasuMap(H: number, W: number, lines: string[]): MasuMap {
+  const masu: MasuMap = new Array(H);
+  for (let i = 0; i < masu.length; i++) {
+    masu[i] = new Array(W);
+  }
+  for (let j = 0; j < H; j++) {
+    const line = lines[j].split("");
+    for (let i = 0; i < W; i++) {
+      if (line[i] === ".") {
+        masu[j][i] = 0;
+      } else {
+        masu[j][i] = 1;
+      }
+    }
+  }
+  return masu;
 }
 
 if (DO_MAIN) {
   const [line0, ...lines] = readInputs();
   const [H, W, X, Y] = line0.split(" ").map(Number);
 
-  //map作り
-  //0 .に対応, 障害物がないことを表す
-  //1 #に対応, 障害物があることを表す
-  const masu: number[][] = new Array();
-  for (let j = 0; j < H; j++) {
-    const line = lines[j].split("");
-    for (let i = 0; i < W; i++) {
-      if (line[i] === ".") {
-        masu[i][j] = 0;
-      } else {
-        masu[i][j] = 1;
-      }
-    }
-  }
+  const masuMap = genMasuMap(H, W, lines);
 }
