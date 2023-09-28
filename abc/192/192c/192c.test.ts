779fd90f24472a08b87fe2bc48ba08fb2e82b28a
koplec <koplec@github.com>
2023-09-28 10:16:27 +0900

    192cテストまで通った

---
+++ abc/192/192c/192c.test.ts
@@ -0,0 +1,44 @@
+import { describe, test, expect } from "@jest/globals";
+import { g1, g2, f, a } from "./192c";
+
+describe("g1", () => {
+  test("g1", () => {
+    expect(g1(1)).toBe(1);
+    expect(g1(12)).toBe(21);
+    expect(g1(21)).toBe(21);
+    expect(g1(8334)).toBe(8433);
+    expect(g1(3021)).toBe(321);
+    expect(g1(314)).toBe(431);
+  });
+
+  test("g2", () => {
+    expect(g2(1)).toBe(1);
+    expect(g2(12)).toBe(12);
+    expect(g2(21)).toBe(12);
+    expect(g2(8334)).toBe(3348);
+    expect(g2(3021)).toBe(123);
+  });
+
+  test("f", () => {
+    expect(f(271)).toBe(594);
+  });
+
+  test("a", () => {
+    expect(a(271, 0)).toBe(271);
+    expect(a(271, 1)).toBe(f(271));
+    expect(a(271, 2)).toBe(f(a(271, 1)));
+    expect(a(273, 5)).toBe(f(a(273, 4)));
+  });
+
+  test("sample01", () => {
+    expect(a(314, 2)).toBe(693);
+  });
+
+  test("sample02", () => {
+    expect(a(1000000000, 100)).toBe(0);
+  });
+
+  test("sample03", () => {
+    expect(a(6174, 100000)).toBe(6174);
+  });
+});
---
+++ abc/192/192c/192c.ts
@@ -0,0 +1,78 @@
+import { assert } from "console";
+import * as fs from "fs";
+
+const DEBUG = false;
+const DO_MAIN = false;
+if (!DEBUG) {
+  console.debug = () => {};
+}
+
+function readInputs(): string[] {
+  const str: string = fs.readFileSync("/dev/stdin", "utf8");
+  console.debug("stdin:", str);
+  const lines: string[] = str.split("\n");
+  return lines;
+}
+
+export function* zip<T>(...args: Array<T>[]) {
+  const length = args[0].length;
+
+  for (let arr of args) {
+    if (arr.length !== length) {
+      throw "Lengths of arrays are not equal";
+    }
+  }
+
+  for (let index = 0; index < length; index++) {
+    let elms = [];
+    for (const arr of args) {
+      elms.push(arr[index]);
+    }
+    yield elms;
+  }
+}
+
+function _g(x: number, compareTo: (x: number, y: number) => number): number {
+  let str = String(x);
+  str = str
+    .split("")
+    .filter((a) => a !== "0")
+    .sort((a, b) => {
+      return compareTo(Number(a), Number(b));
+    })
+    .join("");
+  return Number(str);
+}
+export function g1(x: number): number {
+  return _g(x, (a, b) => b - a);
+}
+
+export function g2(x: number): number {
+  return _g(x, (a, b) => a - b);
+}
+
+export function f(x: number): number {
+  return g1(x) - g2(x);
+}
+/**
+ * k==0のときinitValueそのまま
+ * k==1のとき1回だけ
+ *
+ * @param initValue 初期値
+ * @param kth k番目の値を求める kthは0スタート
+ * @returns k番目の値
+ */
+export function a(initValue: number, kth: number): number {
+  let tmp = initValue;
+  for (let i = 0; i < kth; i++) {
+    tmp = f(tmp);
+  }
+  return tmp;
+}
+
+if (DO_MAIN) {
+  const [line0, ...rem] = readInputs();
+  const [N, K] = line0.split(" ").map(Number);
+  const val = a(N, K);
+  console.log(val);
+}
